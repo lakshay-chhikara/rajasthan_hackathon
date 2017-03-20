34d2181451bff64af838f8e988a74fbde5bf7ab4
karan.garg <karan.garg@snapdeal.com>
2017-03-20 18:58:53 +0530

    Added: Static Content for first time view

---
+++ app/src/main/java/com/example/healthcare/activity/StaticContentActivity.java
@@ -0,0 +1,28 @@
+package com.example.healthcare.activity;
+
+import com.example.healthcare.R;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import butterknife.OnClick;
+
+/**
+ * Created by karangarg on 20/03/17.
+ */
+
+public class StaticContentActivity extends AppCompatActivity {
+
+    @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_static_data);
+    }
+
+    @OnClick(R.id.staticDataButton)
+    public void GoToNextActivity(View v){
+        Intent intent = new Intent(StaticContentActivity.this, TimelineActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/healthcare/activity/TimelineActivity.java
@@ -0,0 +1,20 @@
+package com.example.healthcare.activity;
+
+import com.example.healthcare.R;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by karangarg on 20/03/17.
+ */
+
+public class TimelineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timeline);
+    }
+}
